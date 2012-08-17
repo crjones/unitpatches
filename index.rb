8e38a3667c90cad00ebaba455196b7690eb0ec61
Christopher Jones <crjones@gmail.com>
2012-08-17 18:31:00 -0500

    ignore favicon.ico requests

--- index.rb
+++ index.rb
@@ -1,13 +1,8 @@
 require 'sinatra'
-require 'RMagick'
+
+get '/favicon.ico' do
+end
 
 get '/:numbers' do
-  content_type 'image/png'
-  il = Magick::ImageList.new
-  params[:numbers].each_char do |number|
-    il.push(Magick::Image.read((number + ".gif")))
-	end
-  img = il.append(true)  
-  img.format = 'png'
-  img.to_blob
+  puts params[:numbers]
 end
