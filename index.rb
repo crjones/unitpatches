32ce5eb35f42baa8d63ac6719fcd6e904736497a
Christopher Jones <crjones@gmail.com>
2012-08-17 18:26:40 -0500

    removed first on string

--- index.rb
+++ index.rb
@@ -5,7 +5,7 @@ get '/:numbers' do
   content_type 'image/png'
   il = Magick::ImageList.new
   params[:numbers].each_char do |number|
-    il.push(Magick::Image.read((number + ".gif").first))
+    il.push(Magick::Image.read((number + ".gif")))
 	end
   img = il.append(true)  
   img.format = 'png'
