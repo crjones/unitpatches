27ea64565749d7eda7ff16e5273bf443baa0b973
Christopher Jones <crjones@gmail.com>
2012-08-17 18:23:04 -0500

    Namescape RMagick classes

--- index.rb
+++ index.rb
@@ -3,9 +3,9 @@ require 'RMagick'
 
 get '/:numbers' do
   content_type 'image/png'
-  il = ImageList.new
+  il = Magick::ImageList.new
   params[:numbers].each_char do |number|
-    il.push(Image.read((number + ".gif").first))
+    il.push(Magick::Image.read((number + ".gif").first))
 	end
   img = il.append(true)  
   img.format = 'png'
