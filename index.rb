3ab178bd2bf1f65ece2261d4844d7a7b876e161c
Christopher Jones <crjones@gmail.com>
2012-08-17 18:15:17 -0500

    changed input file type to gif from jpg

--- index.rb
+++ index.rb
@@ -5,7 +5,7 @@ get '/:numbers' do
   content_type 'image/png'
   il = ImageList.new
   params[:numbers].each_char do |number|
-    il.push(Image.read((number + ".jpg").first))
+    il.push(Image.read((number + ".gif").first))
 	end
   img = il.append(true)  
   img.format = 'png'
