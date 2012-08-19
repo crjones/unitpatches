bc0edffe8eef93f190d73469fa98aae76fc776f4
Christopher Jones <crjones@gmail.com>
2012-08-18 23:24:49 -0500

    removed .DS_Store and moved images to directory

--- index.rb
+++ index.rb
@@ -8,7 +8,7 @@ get '/:numbers' do
   content_type 'image/png'
   il = Magick::ImageList.new
   params[:numbers].each_char do |number|
-    il.push(Magick::Image.read(number + ".gif").first)
+    il.push(Magick::Image.read("images/" + number + ".gif").first)
 	end
   img = il.append(false)  
   img.format = 'png'
