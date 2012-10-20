342a9e9548f8d59698e0b09fd6ed470eb74d5d76
Christopher Jones <crjones@gmail.com>
2012-10-20 15:54:26 -0500

    Updated to formats

--- index.rb
+++ index.rb
@@ -20,11 +20,24 @@ end
 get '/favicon.ico' do
 end
 
-get '/:number' do
+get '/:style/:number.:format' do
   content_type 'image/png'
   il = Magick::ImageList.new
   params[:number].each_char do |number|
-    il.push(Magick::Image.read("images/" + number + ".gif").first)
+    il.push(Magick::Image.read("images/" + params[:style] + "/" + number + ".png").first)
+	end
+  img = il.append(false)  
+  img.format = 'png'
+  img.to_blob
+end
+
+get '/:number.:format' do
+  content_type 'image/png'
+  il = Magick::ImageList.new
+  puts params[:number]
+  puts params[:format]
+  params[:number].each_char do |number|
+    il.push(Magick::Image.read("images/red/" + number + ".png").first)
 	end
   img = il.append(false)  
   img.format = 'png'
